afd229ce85a65232479e5810d238adcdd6d7dd4d
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2021-10-08 20:05:04 +0500

    commit2

--- getBalance.js
+++ getBalance.js
@@ -2,3 +2,8 @@ require('dotenv').config();
 const Web3 = require('web3');
 const rpcURL =process.env.INF_URL;
 const web3 = new Web3(rpcURL);
+const account ="0x00000000219ab540356cBB839Cbe05303d7705Fa";
+web3.eth.getBalance(account, (err, wei) => {
+    balance = web3.utils.fromWei(wei, 'ether')
+    console.log(account);
+  })
