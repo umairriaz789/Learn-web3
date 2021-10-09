061f922f68a41d2a1de0a46358f46bff2e32c006
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2021-10-09 16:02:42 +0500

    https://github.com/umairriaz789/Learn-web3

--- getBalance.js
+++ getBalance.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
 const Web3 = require('web3');
-const rpcURL =process.env.INF_URL;
+const rpcURL ="https://ropsten.infura.io/v3/ea5eca6e1a3f4128b92b5b0fc5441ed8";
 const web3 = new Web3(rpcURL);
-const account ="0x00000000219ab540356cBB839Cbe05303d7705Fa";
+const account ="0x4B74b1f52781686e8Df4AF4E25D391732060C527";
 web3.eth.getBalance(account, (err, wei) => {
-    balance = web3.utils.fromWei(wei, 'ether')
-    console.log(account);
-  })
+  if (!err){
+    balance = web3.utils.fromWei(wei, 'ether');
+    console.log(balance);
+  }else {
+    console.log('Error:', err);
+  }
+})
