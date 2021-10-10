7e8c9e02ca351feef3511574b1ab97b79ff43d5d
umairriaz789 <49087501+umairriaz789@users.noreply.github.com>
2021-10-11 01:04:15 +0500

    https://github.com/umairriaz789/Learn-web3

---
+++ utilities.js
@@ -0,0 +1,32 @@
+const _ =require('underscore');
+const Web3 = require('web3');
+const rpcURL ="https://ropsten.infura.io/v3/ea5eca6e1a3f4128b92b5b0fc5441ed8";
+const web3 = new Web3(rpcURL);
+const getaverageGasPrice=async()=>{
+    try {
+        const GasPrice=await web3.eth.getGasPrice();
+        console.log(GasPrice);        
+    } catch (error) {
+        console.log('Error:',error);
+    }
+}
+const Hash=(stringtoEncode)=>{
+    try {
+        console.log('sha3:',web3.utils.sha3('stringtoEncode'));
+        console.log('keccak256:',web3.utils.keccak256('stringtoEncode'));  
+    } catch (error) {
+        console.log('Error:',error);
+        
+    }
+}
+const RandomNumber=(bytes)=>{
+    try {
+        console.log('Number is:',web3.utils.randomHex(bytes));
+    } catch (error) {
+        console.log('Error:',error);
+        
+    }
+}
+//getaverageGasPrice();
+//Hash('UET');
+RandomNumber(2);
